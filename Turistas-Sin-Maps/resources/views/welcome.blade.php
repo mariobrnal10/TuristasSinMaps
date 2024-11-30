ffae9381982c43c389adef7ffc4f2690017d0fec
Arriaga2 <163058678+Arriaga2@users.noreply.github.com>
2024-11-30 16:10:16 -0600

    footer

--- Turistas-Sin-Maps/resources/views/welcome.blade.php
+++ Turistas-Sin-Maps/resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
 @endsection
 
 @section('contenidoInicio')
-<section id="hero" class="hero" style="background-image: url('{{ asset('img/avion2.webp') }}'); background-size: cover; background-position: center; background-repeat: no-repeat; width: 100vw; height: 70vh; position: relative; transition: background-image 1s ease-in-out;">
+<section id="hero" class="hero" style="background-image: url('{{ asset('img/avion2.webp') }}'); background-size: cover; background-position: center; background-repeat: no-repeat; width: 100vw; height: 75vh; position: relative; transition: background-image 1s ease-in-out;">
     <div class="hero-content" style="position: absolute; top: 40%; left: 5%; transform: translateY(-50%); background-color: rgba(255, 255, 255, 0.9); padding: 30px 50px; border-radius: 8px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2); width: 90%; text-align: left;">
         <h2 style="color: #333; font-size: 24px; font-weight: bold; margin-bottom: 10px; text-align: center;">VUELOS Y HOTELES</h2>
         <h1 style="color: #555; font-size: 18px; font-weight: normal; margin-bottom: 20px; text-align: center;">Bienvenido a tu lugar seguro, disfruta con nosotros.</h1>
